7f581ee17cfe34ba5888f15be58c1173313e5bcd
Thiago Gentil <thiagosgentil@gmail.com>
2023-08-14 17:41:01 -0300

    Array form

--- src/app/array-form-component/array-form-component.component.ts
+++ src/app/array-form-component/array-form-component.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-array-form-component',
   templateUrl: './array-form-component.component.html',
-  styleUrls: ['./array-form-component.component.scss']
+  styleUrls: ['./array-form-component.component.scss'],
 })
-export class ArrayFormComponentComponent {
+export class ArrayFormComponentComponent implements OnInit {
+  myForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      email: '',
+      phones: this.fb.array([]),
+    });
+  }
+
+  get phoneForms() {
+    return this.myForm.get('phones') as FormArray;
+  }
+
+  addPhone() {
+    const phone = this.fb.group({
+      area: [],
+      prefix: [],
+      line: [],
+    });
+    this.phoneForms.push(phone);
+  }
+
+  deletePhone(i: number){
+    this.phoneForms.removeAt(i);
+  }
 
 }
