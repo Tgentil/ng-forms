47ecf69d246d97a9b99bb6b5939cfc064547c7d9
Thiago Gentil <thiagosgentil@gmail.com>
2023-08-14 17:06:28 -0300

    Nested Form

--- src/app/nested-form-component/nested-form-component.component.ts
+++ src/app/nested-form-component/nested-form-component.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-nested-form-component',
   templateUrl: './nested-form-component.component.html',
-  styleUrls: ['./nested-form-component.component.scss']
+  styleUrls: ['./nested-form-component.component.scss'],
 })
-export class NestedFormComponentComponent {
+export class NestedFormComponentComponent implements OnInit {
+  myForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    const phone = this.fb.group({
+      area: [],
+      prefix: [],
+      line: [],
+    });
+
+    this.myForm = this.fb.group({
+      email: '',
+      homePhone: phone,
+      cellPhone: phone,
+    });
+  }
 }
