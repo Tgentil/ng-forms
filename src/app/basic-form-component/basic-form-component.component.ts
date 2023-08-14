c78251ae55d0b215bf255153d17a5181ec9cca83
Thiago Gentil <thiagosgentil@gmail.com>
2023-08-14 16:38:55 -0300

    basic form

--- src/app/basic-form-component/basic-form-component.component.ts
+++ src/app/basic-form-component/basic-form-component.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form-component',
   templateUrl: './basic-form-component.component.html',
-  styleUrls: ['./basic-form-component.component.scss']
+  styleUrls: ['./basic-form-component.component.scss'],
 })
-export class BasicFormComponentComponent {
+export class BasicFormComponentComponent implements OnInit {
 
+  myForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      email: '',
+      message: 'default message',
+      career: '',
+    });
+    this.myForm.valueChanges.subscribe(console.log)
+  }
 }
