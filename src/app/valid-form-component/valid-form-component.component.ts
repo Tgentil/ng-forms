7094cad523185f72eb39b0f1704b82c3d1bbc7b4
Thiago Gentil <thiagosgentil@gmail.com>
2023-08-14 18:06:27 -0300

    validators

--- src/app/valid-form-component/valid-form-component.component.ts
+++ src/app/valid-form-component/valid-form-component.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-valid-form-component',
   templateUrl: './valid-form-component.component.html',
-  styleUrls: ['./valid-form-component.component.scss']
+  styleUrls: ['./valid-form-component.component.scss'],
 })
-export class ValidFormComponentComponent {
+export class ValidFormComponentComponent implements OnInit {
+  myForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      email: ['',
+      [
+      Validators.required,
+      Validators.email
+      ]
+        ],
+
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'),
+        ],
+      ],
+
+      age: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.min(18),
+          Validators.max(65),
+        ],
+      ],
+
+      agree: [
+        false,
+        [
+          Validators.requiredTrue
+        ],
+      ],
+
+    });
+  }
+
+  get email() {
+    return this.myForm.get('email');
+  }
+
+  get password() {
+    return this.myForm.get('password');
+  }
+
+  get age() {
+    return this.myForm.get('age');
+  }
 }
