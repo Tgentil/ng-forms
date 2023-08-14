c966205d09fe88032c1d871a8f3a4e8848b848e0
Thiago Gentil <thiagosgentil@gmail.com>
2023-08-14 16:14:51 -0300

    Imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { BasicFormComponentComponent } from './basic-form-component/basic-form-component.component';
 import { NestedFormComponentComponent } from './nested-form-component/nested-form-component.component';
 import { ArrayFormComponentComponent } from './array-form-component/array-form-component.component';
 import { ValidFormComponentComponent } from './valid-form-component/valid-form-component.component';
 import { SubmitFormComponentComponent } from './submit-form-component/submit-form-component.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   declarations: [
@@ -22,7 +31,13 @@ import { SubmitFormComponentComponent } from './submit-form-component/submit-for
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
