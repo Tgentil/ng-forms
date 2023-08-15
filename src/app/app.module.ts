75714c47f6b2a689b9298426ca73c75644ba459e
Thiago Gentil <thiagosgentil@gmail.com>
2023-08-15 13:51:57 -0300

    submmit form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Component-Forms imports
 import { BasicFormComponentComponent } from './basic-form-component/basic-form-component.component';
 import { NestedFormComponentComponent } from './nested-form-component/nested-form-component.component';
 import { ArrayFormComponentComponent } from './array-form-component/array-form-component.component';
@@ -13,11 +14,18 @@ import { SubmitFormComponentComponent } from './submit-form-component/submit-for
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Material imports
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio';
+
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -34,10 +42,14 @@ import { MatChipsModule } from '@angular/material/chips';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     MatInputModule,
+    MatFormFieldModule,
     MatButtonModule,
     MatSelectModule,
     MatCheckboxModule,
-    MatChipsModule
+    MatChipsModule,
+    MatIconModule,
+    MatRadioModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/submit-form-component/submit-form-component.component.ts
+++ src/app/submit-form-component/submit-form-component.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FloatLabelType } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-submit-form-component',
   templateUrl: './submit-form-component.component.html',
   styleUrls: ['./submit-form-component.component.scss']
 })
-export class SubmitFormComponentComponent {
+export class SubmitFormComponentComponent implements OnInit {
 
+  options!: FormGroup;
+  hideRequiredControl = new FormControl(false, Validators.requiredTrue);
+  floatLabelControl = new FormControl('auto' as FloatLabelType, Validators.required);
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.options = this.fb.group({
+      floatLabel: this.floatLabelControl,
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      gender: ['', Validators.required],
+      address: ['', Validators.required],
+      country: ['', Validators.required]
+    });
+  }
+
+  getFloatLabelValue(): FloatLabelType {
+    return this.floatLabelControl.value || 'auto';
+  }
+
+  get firstName() {
+    return this.options.get('firstName');
+  }
+
+  get lastName() {
+    return this.options.get('lastName');
+  }
+
+  get gender() {
+    return this.options.get('gender');
+  }
+
+  get address() {
+    return this.options.get('address');
+  }
+
+  get country() {
+    return this.options.get('country');
+  }
 }
