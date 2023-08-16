9a250c26b685c806eb3042e1b494be277d51daf8
Thiago Gentil <thiagosgentil@gmail.com>
2023-08-16 12:55:37 -0300

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { BasicFormComponentComponent } from './basic-form-component/basic-form-component.component';
+import { NestedFormComponentComponent } from './nested-form-component/nested-form-component.component';
+import { ArrayFormComponentComponent } from './array-form-component/array-form-component.component';
+import { ValidFormComponentComponent } from './valid-form-component/valid-form-component.component';
+import { SubmitFormComponentComponent } from './submit-form-component/submit-form-component.component';
+
+const routes: Routes = [
+  { path: 'basic-form', component: BasicFormComponentComponent },
+  { path: 'nested-form', component: NestedFormComponentComponent },
+  { path: 'array-form', component: ArrayFormComponentComponent },
+  { path: 'valid-form', component: ValidFormComponentComponent },
+  { path: 'submit-form', component: SubmitFormComponentComponent },
+  { path: '', redirectTo: '/submit-form', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
