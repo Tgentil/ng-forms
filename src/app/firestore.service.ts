7e71625e1bfeae90576fa4a403444c50814b3aff
Thiago Gentil <thiagosgentil@gmail.com>
2023-08-16 11:31:29 -0300

    creating a service

---
+++ src/app/firestore.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+
+  constructor(private firestore: AngularFirestore) { }
+
+  addUser(data: any) {
+    return this.firestore.collection('users').add(data);
+  }
+
+}
